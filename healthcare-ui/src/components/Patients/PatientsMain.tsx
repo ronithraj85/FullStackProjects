04160007ff74f43b1e0b13546a1cb4f6fb5391b8
Ronith Raj <ronithrajkopperla@gmail.com>
2026-01-01 19:06:12 +0530

    Patient crud integration and changes.

--- healthcare-ui/src/components/Patients/PatientsMain.tsx
+++ healthcare-ui/src/components/Patients/PatientsMain.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { getPatients } from "../../services/Auth.service";
+import {
+  deletePatient,
+  getPatients,
+  updatePatient,
+} from "../../services/Auth.service";
+import type { PatientResponseDto } from "../../types/PatientResponseDto";
+import AddPatient from "./AddPatient";
 
 const PatientsMain = () => {
   const [addPatient, setAddPatient] = useState(false);
-  const [patientAdded, setPatientAdded] = useState("");
   const [patientEdited, setPatientEdit] = useState("");
   const [patientDeleted, setPatientDeleted] = useState("");
   const [patients, setPatients] = useState<PatientResponseDto>();
   const [loading, setLoading] = useState(true);
+  const [editingPatient, setEditingPatient] =
+    useState<PatientResponseDto | null>(null);
+  const [formData, setFormData] = useState<Partial<PatientResponseDto>>({});
 
   useEffect(() => {
     const fetchPatients = async () => {
@@ -22,6 +30,40 @@ const PatientsMain = () => {
     fetchPatients();
   }, []);
 
+  const handleDelete = async (id: number) => {
+    try {
+      await deletePatient(id);
+      setPatients((prev) => prev?.filter((u) => u.id !== id));
+      setPatientDeleted("Patient removed successfully");
+      setTimeout(() => {
+        setPatientDeleted("");
+      }, 3000);
+    } catch (err) {
+      console.log("Error deleting the patient", e);
+    }
+  };
+
+  const handleEdit = (patient: PatientResponseDto) => {
+    setEditingPatient(patient);
+    setFormData(patient);
+  };
+
+  const handleSave = async () => {
+    try {
+      await updatePatient(editingPatient?.id!, formData);
+      setPatients(
+        patients?.map((p) =>
+          p.id === editingPatient?.id ? { ...p, ...formData } : p
+        )
+      );
+      setPatientEdit("Patient updated successfully!");
+      setEditingPatient(null);
+      setTimeout(() => setPatientEdit(""), 3000);
+    } catch (err) {
+      console.log("Error updating patient:", err);
+    }
+  };
+
   return (
     <div className="p-8">
       <div className="flex justify-between">
@@ -34,14 +76,9 @@ const PatientsMain = () => {
         </button>
       </div>
       <div className="overflow-x-auto shadow-lg rounded-lg">
-        {patientAdded && (
-          <div className="mb-4 p-3 bg-green-200 text-green-800 rounded">
-            {" "}
-            {patientAdded}{" "}
-          </div>
-        )}
+        {" "}
         {patientDeleted && (
-          <div className="mb-4 p-3 bg-green-100 text-green-800 rounded">
+          <div className="mb-4 p-3 bg-orange-100 text-orange-600 rounded">
             {patientDeleted}
           </div>
         )}
@@ -50,7 +87,6 @@ const PatientsMain = () => {
             {patientEdited}
           </div>
         )}
-
         <table className="min-w-full bg-white border border-gray-200">
           <thead className="bg-blue-600 text-white">
             <tr>
@@ -63,38 +99,50 @@ const PatientsMain = () => {
           </thead>
           {loading && <p className="text-center">Fetching Patients List!</p>}
           <tbody>
-            {patients.map((patient, idx) => (
-              <tr
-                key={idx}
-                className={idx % 2 === 0 ? "bg-gray-50" : "bg-white"}
-              >
-                <td className="py-3 px-4">{patient.name}</td>
-                <td className="py-3 px-4">{patient.email}</td>
-                <td className="py-3 px-4">{patient.mobile}</td>
-                <td className="py-3 px-4">{patient.dateOfBirth}</td>
-
-                <td className="py-3 px-4 space-x-2">
-                  <button
-                    // onClick={() => handleDelete(user.id)}
-                    className="bg-blue-500 hover:bg-blue-700 text-white px-3 py-1 rounded"
-                  >
-                    Delete
-                  </button>
-                  <button
-                    // onClick={() => handleEdit(user)}
-                    className="bg-gray-500 hover:bg-gray-600 text-white px-3 py-1 rounded"
-                  >
-                    Edit
-                  </button>
-                </td>
+            {patients?.filter((patient) => patient.active).length === 0 ? (
+              <tr>
+                {" "}
+                <td colSpan={5} className="text-center py-4 text-gray-600">
+                  {" "}
+                  No patients found{" "}
+                </td>{" "}
               </tr>
-            ))}
+            ) : (
+              patients
+                ?.filter((patient) => patient.active)
+                .map((patient, idx) => (
+                  <tr
+                    key={patient.id}
+                    className={idx % 2 === 0 ? "bg-gray-50" : "bg-white"}
+                  >
+                    <td className="py-3 px-4">{patient.name}</td>
+                    <td className="py-3 px-4">{patient.email}</td>
+                    <td className="py-3 px-4">{patient.mobile}</td>
+                    <td className="py-3 px-4">{patient.dateOfBirth}</td>
+
+                    <td className="py-3 px-4 space-x-2">
+                      <button
+                        onClick={() => handleDelete(patient.id)}
+                        className="bg-blue-500 hover:bg-blue-700 text-white px-3 py-1 rounded"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => handleEdit(patient)}
+                        className="bg-gray-500 hover:bg-gray-600 text-white px-3 py-1 rounded"
+                      >
+                        Edit
+                      </button>
+                    </td>
+                  </tr>
+                ))
+            )}
           </tbody>
         </table>
-        {/* {addUser && <AddAdminPage />}
-        {editingUser && (
+        {addPatient && <AddPatient />}
+        {editingPatient && (
           <div className="mt-6 p-4 border rounded bg-gray-50">
-            <h3 className="text-lg font-semibold mb-4">Edit Existing User</h3>
+            <h3 className="text-lg font-semibold mb-4">Edit Patient</h3>
             <input
               type="text"
               value={formData.name || ""}
@@ -106,20 +154,28 @@ const PatientsMain = () => {
             />
             <input
               type="text"
-              value={formData.username || ""}
-              onChange={(e) =>
-                setFormData({ ...formData, username: e.target.value })
-              }
-              placeholder="UserName"
-              className="border p-2 mr-2"
-            />
-            <input
-              type="text"
               value={formData.email || ""}
               onChange={(e) =>
                 setFormData({ ...formData, email: e.target.value })
               }
               placeholder="Email"
+              className="border p-2 mr-2"
+            />
+            <input
+              type="text"
+              value={formData.mobile || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, mobile: e.target.value })
+              }
+              placeholder="Mobile"
+              className="border p-2 mr-2"
+            />
+            <input
+              type="date"
+              value={formData.dateOfBirth || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, dateOfBirth: e.target.value })
+              }
               className="border p-2 mr-2"
             />
             <button
@@ -129,13 +185,13 @@ const PatientsMain = () => {
               Save
             </button>
             <button
-              onClick={() => setEditingUser(null)}
+              onClick={() => setEditingPatient(null)}
               className="bg-gray-400 hover:bg-gray-500 text-white px-3 py-1 rounded ml-2"
             >
               Cancel
             </button>
           </div>
-        )} */}
+        )}
       </div>
     </div>
   );
--- healthcare-ui/src/services/Auth.service.tsx
+++ healthcare-ui/src/services/Auth.service.tsx
@@ -169,7 +169,7 @@ export const deleteDoctor = async (id: number): Promise<void> => {
 // Get all the patients
 export const getPatients = async (): Promise<PatientResponseDto[]> => {
   try {
-    const res = await axios.get<PatientResponseDto[]>(`${API_PATIENT_URL}/`, {
+    const res = await axios.get<PatientResponseDto[]>(`${API_PATIENT_URL}`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
@@ -178,6 +178,66 @@ export const getPatients = async (): Promise<PatientResponseDto[]> => {
     return res.data; // return response data to caller
   } catch (err) {
     console.error("Error occurred in fetchPatients:", err);
+    throw err;
+  }
+};
+
+//Adding a patient
+export const addPatient = async (
+  name: string,
+  email: string,
+  mobile: string,
+  dateOfBirth: string
+) => {
+  try {
+    const result = await axios.post(
+      `${API_PATIENT_URL}`,
+      {
+        name,
+        email,
+        mobile,
+        dateOfBirth,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    );
+    return result.data;
+  } catch (err) {
+    console.log("Error while registering the patient-", err);
+    throw err;
+  }
+};
+
+// Update user service
+export const updatePatient = async (
+  id: number,
+  updatedData: Partial<PatientResponseDto>
+): Promise<PatientResponseDto> => {
+  const res = await axios.put<UserRespoPatientResponseDtonseDto>(
+    `${API_PATIENT_URL}/${id}`,
+    updatedData,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }
+  );
+  return res.data; // updated user object
+};
+
+// Delete patient service
+export const deletePatient = async (id: number): Promise<void> => {
+  try {
+    await axios.delete(`${API_PATIENT_URL}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    });
+  } catch (err) {
+    console.error("Error occurred while deleting user:", err);
     throw err;
   }
 };
